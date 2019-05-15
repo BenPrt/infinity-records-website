79a76726e35c0148ddd1c334f95d2159552a6a99
Ben Prt <lifaen596@hotmail.fr>
2019-05-15 13:46:18 +0200

    Code correction, everything is OK

--- src/app/contact/contact-page.component.ts
+++ src/app/contact/contact-page.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
-  templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss'],
+  templateUrl: './contact-page.component.html',
+  styleUrls: ['./contact-page.component.scss'],
 })
 export class ContactPageComponent {
   constructor() {}
--- src/app/home/home-page.component.ts
+++ src/app/home/home-page.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'],
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.scss'],
 })
 export class HomePageComponent {
   constructor() {}
