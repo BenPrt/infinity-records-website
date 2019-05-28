b453148af90413a2ad197596cf3a1bcb2f7f9521
Ben Prt <lifaen596@hotmail.fr>
2019-05-28 13:59:40 +0200

    Header OK without loading animation or sticky animation

--- src/app/layout/header/header-desktop-menu/header-desktop-menu.component.ts
+++ src/app/layout/header/header-desktop-menu/header-desktop-menu.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { Location } from '@angular/common';
 
+import { trigger, transition, style, animate } from '@angular/animations';
+
 @Component({
   selector: 'header-desktop-menu-component',
   templateUrl: './header-desktop-menu.component.html',
   styleUrls: ['./header-desktop-menu.component.scss'],
+  animations: [
+    trigger('indicatorFade', [
+      transition(':enter', [style({ opacity: 0 }), animate('200ms', style({ opacity: 1 }))]),
+      transition(':leave', [style({ opacity: 1 }), animate('200ms', style({ opacity: 0 }))]),
+    ]),
+  ],
 })
 export class HeaderDesktopMenuComponent {
   constructor(private location: Location) {}
