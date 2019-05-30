5377832a2bea87027d99480f443a867457da7973
Ben Prt <lifaen596@hotmail.fr>
2019-05-30 17:05:18 +0200

    Desktop contact form nearly perfect

--- src/app/layout/footer/contact-form/contact-form.component.ts
+++ src/app/layout/footer/contact-form/contact-form.component.ts
@@ -3,16 +3,25 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { environment } from 'src/environments/environment';
 import { ContactService } from 'src/app/shared/services/contact.service';
 import { Email } from 'src/app/models/e-mail';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'contact-form-component',
   templateUrl: './contact-form.component.html',
   styleUrls: ['./contact-form.component.scss'],
-  animations: [],
+  animations: [
+    trigger('submitButtonFade', [
+      transition(':leave', [style({ opacity: 1 }), animate('300ms', style({ opacity: 0 }))]),
+    ]),
+    trigger('successMessageFade', [
+      transition(':enter', [style({ opacity: 0 }), animate('300ms', style({ opacity: 1 }))]),
+    ]),
+  ],
 })
 export class ContactFormComponent {
   contactFormGroup: FormGroup;
   recaptchaToken: string = environment.contactFormRecaptchaKey;
+  formHasBeenSubmitted: boolean = false;
 
   constructor(private formBuilder: FormBuilder, private contactService: ContactService) {}
 
@@ -33,8 +42,7 @@ export class ContactFormComponent {
     this.contactFormGroup.disable();
     const email: Email = this.contactFormGroup.value;
     this.contactService.sendEmail(email).subscribe((res) => {
-      console.log(res);
-      console.log('E-mail request sent to apis');
+      this.formHasBeenSubmitted = true;
     });
   }
 
