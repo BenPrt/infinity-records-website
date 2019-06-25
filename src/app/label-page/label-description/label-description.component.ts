1dc2a878aff911fdf4fce5276484a4a1a8e1d54e
Ben Prt <lifaen596@hotmail.fr>
2019-06-25 19:18:56 +0200

    Done with mobile version

--- src/app/label-page/label-description/label-description.component.ts
+++ src/app/label-page/label-description/label-description.component.ts
@@ -1,16 +1,31 @@
-import { Component, Inject, PLATFORM_ID } from '@angular/core';
+import { Component, Inject, PLATFORM_ID, OnInit, AfterViewInit, OnDestroy } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
+import { Subscription } from 'rxjs';
+import { DeviceService } from 'src/app/shared/services/device.service';
 
 @Component({
   selector: 'label-description',
   templateUrl: './label-description.component.html',
   styleUrls: ['./label-description.component.scss'],
 })
-export class LabelDescriptionComponent {
+export class LabelDescriptionComponent implements OnInit, AfterViewInit, OnDestroy {
   isBrowser: boolean;
+  isMobile: boolean;
+  deviceTypeSubscription: Subscription;
 
-  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+  constructor(@Inject(PLATFORM_ID) private platformId: Object, private deviceService: DeviceService) {
     this.isBrowser = isPlatformBrowser(platformId);
+  }
+
+  ngOnInit(): void {
+    this.initDeviceType();
+  }
+
+  initDeviceType(): void {
+    this.isMobile = this.deviceService.getIsMobile();
+    this.deviceTypeSubscription = this.deviceService.deviceIsMobileHasChanged.subscribe((isMobile: boolean) => {
+      this.isMobile = isMobile;
+    });
   }
 
   ngAfterViewInit(): void {
@@ -19,10 +34,22 @@ export class LabelDescriptionComponent {
 
   initcontainerHeight(): void {
     if (this.isBrowser) {
-      const totalHeight = document.getElementById('left-content').offsetHeight;
-      document.getElementById('description-left-layer').style.height = `${totalHeight - 56}px`;
-      document.getElementById('description-right-layer').style.height = `calc(${totalHeight -
-        56}px - 80px - (20.9vw / 2))`;
+      const leftHeight = document.getElementById('left-content').offsetHeight;
+      if (!this.isMobile) {
+        document.getElementById('description-left-layer').style.height = `${leftHeight - 56}px`;
+        document.getElementById('description-right-layer').style.height = `calc(${leftHeight -
+          56}px - 80px - (20.9vw / 2))`;
+      } else {
+        document.getElementById('right-separator').style.marginTop = `${leftHeight + 64 + 48}px`;
+        const rightHeight = document.getElementById('right-description').offsetHeight;
+        const totalHeight = leftHeight + 64 + 48 + rightHeight + 72;
+        document.getElementById('description-left-layer').style.height = `${totalHeight}px`;
+        document.getElementById('description-right-layer').style.height = `${totalHeight}px`;
+      }
     }
+  }
+
+  ngOnDestroy(): void {
+    this.deviceTypeSubscription.unsubscribe();
   }
 }
