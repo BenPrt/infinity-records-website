f0cc3f094a807391abb57203ca7d0202e231c2a1
Ben Prt <lifaen596@hotmail.fr>
2019-06-25 16:43:01 +0200

    Desktop description nearly finished

--- src/app/label-page/label-description/label-description.component.ts
+++ src/app/label-page/label-description/label-description.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'label-description',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./label-description.component.scss'],
 })
 export class LabelDescriptionComponent {
-  constructor() {}
+  isBrowser: boolean;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+    this.isBrowser = isPlatformBrowser(platformId);
+  }
+
+  ngAfterViewInit(): void {
+    this.initcontainerHeight();
+  }
+
+  initcontainerHeight(): void {
+    if (this.isBrowser) {
+      const totalHeight = document.getElementById('left-content').offsetHeight;
+      document.getElementById('description-left-layer').style.height = `${totalHeight - 56}px`;
+      document.getElementById('description-right-layer').style.height = `calc(${totalHeight -
+        56}px - 80px - (20.9vw / 2))`;
+    }
+  }
 }
