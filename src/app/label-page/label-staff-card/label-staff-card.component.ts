c13c05a4f99522aecb90294b513dde11293e0c2c
Ben Prt <lifaen596@hotmail.fr>
2019-06-26 11:10:37 +0200

    Prepared staff preview desktop container, added staff card components

---
+++ src/app/label-page/label-staff-card/label-staff-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'label-staff-card',
+  templateUrl: './label-staff-card.component.html',
+  styleUrls: ['./label-staff-card.component.scss'],
+})
+export class LabelStaffCardComponent {
+  constructor() {}
+}
--- src/app/label-page/label-staff/label-staff.component.ts
+++ src/app/label-page/label-staff/label-staff.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, OnDestroy, OnInit, Inject, PLATFORM_ID } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { DeviceService } from 'src/app/shared/services/device.service';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'label-staff',
   templateUrl: './label-staff.component.html',
   styleUrls: ['./label-staff.component.scss'],
 })
-export class LabelStaffComponent {
-  constructor() {}
+export class LabelStaffComponent implements OnInit, AfterViewInit, OnDestroy {
+  isBrowser: boolean;
+  isMobile: boolean;
+  deviceTypeSubscription: Subscription;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object, private deviceService: DeviceService) {
+    this.isBrowser = isPlatformBrowser(platformId);
+  }
+
+  ngOnInit(): void {
+    this.initDeviceType();
+  }
+
+  initDeviceType(): void {
+    this.isMobile = this.deviceService.getIsMobile();
+    this.deviceTypeSubscription = this.deviceService.deviceIsMobileHasChanged.subscribe((isMobile: boolean) => {
+      this.isMobile = isMobile;
+    });
+  }
+
+  ngAfterViewInit(): void {
+    this.initcontainerHeight();
+  }
+
+  initcontainerHeight(): void {
+    if (this.isBrowser) {
+      const secondLayerHeight = document.getElementById('staff-second-layer').offsetHeight;
+      if (!this.isMobile) {
+        document.getElementById('staff-first-layer').style.height = `${secondLayerHeight}px`;
+      } else {
+
+      }
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.deviceTypeSubscription.unsubscribe();
+  }
 }
