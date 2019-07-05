414526718969cf15caa1af8cc2e30f14f7534918
Ben Prt <lifaen596@hotmail.fr>
2019-07-05 09:46:02 +0200

    Desktop artist banner done

--- src/app/artists-page/artist-page/artist-banner/artist-banner.component.ts
+++ src/app/artists-page/artist-page/artist-banner/artist-banner.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { ArtistInformations } from 'src/app/models/artists-info';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'artist-banner',
@@ -9,5 +10,10 @@ import { ArtistInformations } from 'src/app/models/artists-info';
 export class ArtistBannerComponent {
   @Input() artist: ArtistInformations;
 
-  constructor() {}
+  constructor(private router: Router) {}
+
+  navigateTo(url: string) {
+    console.log(url);
+    this.router.navigateByUrl(url);
+  }
 }
