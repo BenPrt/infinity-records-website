457e0da6ca507b6e66ce304ef899cc6c6608a2a2
Ben <parant.b@gmail.com>
2019-09-23 17:26:34 +0200

    Commit to test changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -194,6 +194,7 @@ export class AppComponent implements OnInit, AfterViewInit {
 
   initAnimationState(): void {
     const path = this.location.path();
+    alert(path);
     if (path === '' || path.substring(0, 1) === '?') {
       this.initMenuAnimation();
       this.defineMetadata();
