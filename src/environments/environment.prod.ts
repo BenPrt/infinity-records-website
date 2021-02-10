885f98164b90ba31fca9302e91ea5a7b27d9d298
Benjamin Parant <bparant@serti.com>
2021-02-10 11:49:26 -0500

    Updated environment variable for sending emails

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl : 'https://api.infinity-records.fr',
+  apiUrl : 'https://www.api.infinity-records.fr',
   contactFormRecaptchaKey: '6LeZs6QUAAAAACihintSSAc_48B3Llh3kclMBFAV',
 };
